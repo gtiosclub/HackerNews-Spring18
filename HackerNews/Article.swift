1ba4cde3313ddda4fdf8fffbb57b349c54bb45f4
Numeric <vista980622@gmail.com>
2018-03-06 12:57:00 -0500

    Use Decodable for Article

--- HackerNews/Article.swift
+++ HackerNews/Article.swift
@@ -1,23 +1,40 @@
 import Foundation
 
-struct Article: Codable
+struct Article
 {
     var id: Int
     var title: String
     var url: String
     var score: Int
-    var time: Double
     var kids: [Int]
+    var time: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case url
+        case score
+        case kids
+        case time
+    }
 }
 
-extension Double {
-    func getDateStringFromUTC() -> String {
-        let date = Date(timeIntervalSince1970: self)
+extension Article: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        title = try values.decode(String.self, forKey: .title)
+        url = try values.decode(String.self, forKey: .url)
+        score = try values.decode(Int.self, forKey: .score)
+        kids = try values.decode([Int].self, forKey: .kids)
         
+        let dateUTC = try values.decode(Double.self, forKey: .time)
+        let date = Date(timeIntervalSince1970: dateUTC)
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US")
         dateFormatter.dateStyle = .medium
-        
-        return dateFormatter.string(from: date)
+        time = dateFormatter.string(from: date)
     }
+    
 }
+
